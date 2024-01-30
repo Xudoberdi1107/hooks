93073fddbceea457b76584c983ff889d048663c8
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-01-31 01:17:32 +0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import Logo from "/logo.jpeg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Khudoberdi</h1>
